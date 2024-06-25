38664a154836f7601cb1bdd2f9034ae5cfef5779
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-25 15:48:59 +0200

    feat: linked Models OneToMany - Type.php&Project.php

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,7 +9,12 @@ class Project extends Model
 {
 
     use HasFactory;
-    
+
     protected $fillable = ['title', 'slug', 'description'];
 
+    public function type()
+    {
+
+        return $this->belongsTo(Type::class);
+    }
 }
