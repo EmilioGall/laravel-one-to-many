a11fc8f2456bbda6d81813cae9cbc9a3fba5ea36
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-25 15:44:33 +0200

    feat: created Model - Type.php & created and executed Migration - add_type_id_foreign_key_to_projects_table.php

---
+++ database/migrations/2024_06_25_132855_add_type_id_foreign_key_to_projects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     * ALTER TABLE `projects`
+     * ADD `type_id` BIGINT UNSIGNED NULLABLE
+     *  AFTER `title`;
+     * 
+     * FOREIGN KEY `type_id` REFERENCES types(`id`);
+     * 
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('type_id')->nullable()->after('title');
+
+            $table->foreign('type_id')->references('id')->on('types');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            $table->dropForeign('projects_type_id_foreign');
+
+            $table->dropColumn('type_id');
+
+        });
+    }
+};
